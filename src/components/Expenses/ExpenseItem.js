ba42aad9523320c62a36880fc7e2faef82715528
Miguel Orta <supermanorta@gmail.com>
2022-07-05 18:10:23 -0500

    praticing a componets state being changed.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,29 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './Expenseltem.css';
 
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const dummyClickHandler=()=>{
+    setTitle('UpdatedTitle');
+    console.log('DummyClickHandler');
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className="expense-item_description">
-        <h2> {props.title} </h2>{" "}
+        <h2> {title} </h2>{" "}
         <div className="expense-item__price">${props.amount} </div>{" "}
-      </div>{" "}
+      </div>
+      <button onClick={dummyClickHandler}>Change Title</button> 
+      {/* by writing the above statement as so, we are creating a pointer to the function 
+      which waits for the action to occur.
+      We could call it as a function but this would cause the function to fire as soon as the 
+      pages loads, which here, we do not want. */}
     </Card>
   );
 }
