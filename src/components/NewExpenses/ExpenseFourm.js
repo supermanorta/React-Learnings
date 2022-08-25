4b2d69502dd086975e4d4d9a12c3889036080a7e
supermanorta <supermanorta@yahoo.com>
2022-08-24 21:28:34 -0500

    Updated expense fourm to use seprate usesState to populate title, amount and date. I have also updated the page to use two way binding, and so that data is cleared after the user clicks 'add Expense'

--- src/components/NewExpenses/ExpenseFourm.js
+++ src/components/NewExpenses/ExpenseFourm.js
@@ -2,17 +2,18 @@ import React, {useState} from "react";
 import './ExpenseFourm.css'
 
 const ExpenseFourm = () =>{
-    // const[enteredTitle,setTitle] = useState('');
-    // const[enterAmount,setAmount] = useState('');
-    // const[enterDate,setDate] = useState('');
+    const[enteredTitle,setTitle] = useState('');
+    const[enterAmount,setAmount] = useState('');
+    const[enterDate,setDate] = useState('');
 // call it once, and pass a object as value, and we can group together the 3 states above
-   const [userInput, setUserInput] = useState({
-        enteredTitle:'',
-        enterAmount:'',
-        enterDate:''
-    })
+  //  const [userInput, setUserInput] = useState({
+  //       enteredTitle:'',
+  //       enterAmount:'',
+  //       enterDate:''
+  //   })
 
     const titleChangeHandler =(event) => {
+      setTitle(event.target.value);
         // this will updat the state, but you could get conflicting state of title, given that React schedules state updates, and might pullone that is dated.
     //   setUserInput({
     //     enteredTitle: event.target.value,
@@ -22,37 +23,50 @@ const ExpenseFourm = () =>{
     console.log(event);
     // console.log('Title changed');
     // below will always give you the latest state of the function, since React scheudle updates when you want it updated.
-        setUserInput((prevState) => {
-          return { ...prevState, enteredTitle: event.target.value };
-      });
-    };
+    //     setUserInput((prevState) => {
+    //       return { ...prevState, enteredTitle: event.target.value };
+    //   });
+     };
     const amountHandler=( event )=>{
+      setAmount(event.target.value);
         // setUserInput({
         // ...userInput,
         // enterAmount: event.target.value})
-        setUserInput((prevState) => {
-        return { ...prevState, enterAmount: event.target.value };
-        });
+        // setUserInput((prevState) => {
+        // return { ...prevState, enterAmount: event.target.value };
+        // });
     };
     const dateHandler=( event )=>{
+      setDate(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   enterDate: event.target.value,
     // });
 
-        setUserInput((prevState) => {
-        return { ...prevState, enterDate: event.target.value };
-        });
+        // setUserInput((prevState) => {
+        // return { ...prevState, enterDate: event.target.value };
+        // });
         };
 
     const submitHandler =(event) => {
-        event.preventDefault();
+        event.preventDefault();//prevent the defualt request from being sent
+
         const expenseData = {
-          title: userInput.enteredTitle,
-          amount: userInput.enterAmount,
-          date: new Date(userInput.enterDate),
+          // title: userInput.enteredTitle,
+          // amount: userInput.enterAmount,
+          // date: new Date(userInput.enterDate),
+          title: enteredTitle,
+          amount: enterAmount,
+          date: new Date(enterDate)
         };
         console.log(expenseData);
+        //now clear the data after we have accepted it from user input by..
+        setTitle('');
+        setAmount('');
+        setDate('');
+        // setUserInput(event.target.value);
+        // setUserInput(event.target.value);
+        // setUserInput(event.target.value);
     };
 
     return (
@@ -60,7 +74,10 @@ const ExpenseFourm = () =>{
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler}></input>
+            <input type="text" 
+            value={enteredTitle}//value={userInput.enteredTitle}
+            onChange={titleChangeHandler}>  
+            </input>
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
@@ -68,6 +85,7 @@ const ExpenseFourm = () =>{
               type="number"
               min="0.01"
               step="0.01"
+              value={enterAmount}
               onChange={amountHandler}
             ></input>
           </div>
@@ -77,6 +95,7 @@ const ExpenseFourm = () =>{
               type="date"
               min="2019-01-01"
               max="2022-12-31"
+              value={enterDate}
               onChange={dateHandler}
             ></input>
           </div>
