6e3cf7c1fbd0d992678b4a5d1dd81bd5ef4105cf
supermanorta <supermanorta@yahoo.com>
2022-09-12 18:06:09 -0500

    outsource the conditional logic for Expense List change based off the year selected. I also left space for <li> tags to be added to ExpenseList class, however when I try to change this I am seeing bullet points in the UI and will have to research this more before updating the symantics of the code.

---
+++ src/components/Expenses/ExpenseList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+const ExpenseList = props => {
+    if (props.expensesdata.length ==0){
+        return <h2 className="expense-list__fallback">Found no expenses</h2>;
+    }
+    
+      return (
+        <ul className="expenses-list">
+          {props.expensesdata.map((expense) => (
+            <ExpenseItem
+              key={expense.id} // adding a id to a list of items helps react know where the item is,how long a list of items is, and there for does not have to work as hard to identify it.
+              title={expense.title} // adding a id will also help react update the list faster
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+        </ul>
+      );
+};
+
+export default ExpenseList;
